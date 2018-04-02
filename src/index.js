21ea7a7c1dbb785cfca76118de958fa1e434c233
Vishnu Varthan <vvarthan7@gmail.com>
2018-04-02 12:06:02 +0530

    changed file names

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import WeatherApp from './WeatherApp';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<WeatherApp />, document.getElementById('root'));
 registerServiceWorker();
